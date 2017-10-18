843d3b643b4d6f80753f35fc51bfaf04fcad5d88
Joel Sharbrough <jsharbrough@users.noreply.github.com>
2017-10-18 12:08:06 -0600

    Add files via upload

--- coverage.py
+++ coverage.py
@@ -30,35 +30,53 @@ def coverage(vcfFile,regionsFile=False):
         indexFile = open(vcfFile + '.pickle', 'rb')
     depthDict = pickle.load(indexFile)
     indexFile.close()
-    try:
-        infile = open(regionsFile,'r')
-        sys.stdout.write('Gene\tScaffold\tStart\tStop\tMean Depth\tMedian Depth\n')
-        for line in infile:
-            newLine = line
-            while newLine[-1] == '\n' or newLine[-1] == '\t' or newLine[-1] == '\r':
-                newLine = newLine[0:-1]
-            lineSplit = newLine.split('\t')
-            gene = lineSplit[0]
-            scaffold = lineSplit[1]
-            start = int(lineSplit[2])
-            stop = int(lineSplit[3])
+    if regionsFile != False:
+        try:
+            infile = open(regionsFile,'r')
+            sys.stdout.write('Gene\tScaffold\tStart\tStop\tMean Depth\tMedian Depth\n')
+            for line in infile:
+                newLine = line
+                while newLine[-1] == '\n' or newLine[-1] == '\t' or newLine[-1] == '\r':
+                    newLine = newLine[0:-1]
+                lineSplit = newLine.split('\t')
+                gene = lineSplit[0]
+                scaffold = lineSplit[1]
+                start = int(lineSplit[2])
+                stop = int(lineSplit[3])
+                depths = []
+                i = start
+                scaffDepths = depthDict[scaffold]
+                totalDepth = 0
+                while i < stop:
+                    posDepth = scaffDepths[i]
+                    totalDepth += posDepth
+                    depths.append(posDepth)
+                    i += 1
+                depths.sort()
+                meanDepth = totalDepth/(float(stop - start))
+                if len(depths)%2 == 0:
+                    median = float(depths[len(depths)/2] + depths[len(depths)/2 - 1])/2.0
+                else:
+                    median = depths[len(depths)/2]
+                sys.stdout.write(gene + '\t' + scaffold + '\t' + str(start) + '\t' + str(stop) + '\t' + str(meanDepth) + '\t' + str(median) + '\n')
+        except IOError:
+            sys.stdout.write('Mean Depth\tMedian Depth\n')
             depths = []
-            i = start
-            scaffDepths = depthDict[scaffold]
             totalDepth = 0
-            while i < stop:
-                posDepth = scaffDepths[i]
-                totalDepth += posDepth
-                depths.append(posDepth)
-                i += 1
+            for scaffold in depthDict:
+                scaffDict = depthDict[scaffold]
+                for pos in scaffDict:
+                    depth = scaffDict[pos]
+                    depths.append(depth)
+                    totalDepth += depth
             depths.sort()
-            meanDepth = totalDepth/(float(stop - start))
+            meanDepth = totalDepth/(float(len(depths)))
             if len(depths)%2 == 0:
                 median = float(depths[len(depths)/2] + depths[len(depths)/2 - 1])/2.0
             else:
                 median = depths[len(depths)/2]
-            sys.stdout.write(gene + '\t' + scaffold + '\t' + str(start) + '\t' + str(stop) + '\t' + str(meanDepth) + '\t' + str(median) + '\n')
-    except IOError:
+            sys.stdout.write(str(meanDepth) + '\t' + median + '\n')
+    else:
         sys.stdout.write('Mean Depth\tMedian Depth\n')
         depths = []
         totalDepth = 0
@@ -75,6 +93,7 @@ def coverage(vcfFile,regionsFile=False):
         else:
             median = depths[len(depths)/2]
         sys.stdout.write(str(meanDepth) + '\t' + median + '\n')
+
 
 def help():
     helpStatement = 'USAGE\n\n\tpython coverage.py foo.vcf regions.txt > coverage.txt\n'
